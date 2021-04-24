881d96daa64e653fc5694a96c88953c2a6451912
Sterling Tamboline <stamboli@sfu.ca>
2021-04-24 15:04:22 -0700

    Added routes for home and questionnaire page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from 'react';
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./Pages/Home/Home";
+import Questionnaire from "./Pages/Questionnaire/Questionnaire";
+
 function App() {
   return (
     <div className="App">
-
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Questionnaire" component={Questionnaire} />
+      </Switch>
     </div>
   );
 }
