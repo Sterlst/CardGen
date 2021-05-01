35722cf613e1ae29fd16fa60d70d645090a59725
btambo <70358548+btambo@users.noreply.github.com>
2021-04-30 20:51:22 -0700

    Finished Styling for Home Screen Message

--- src/Pages/Home/WelcomeMessage.js
+++ src/Pages/Home/WelcomeMessage.js
@@ -5,8 +5,11 @@ const WelcomeMessage = () => {
     return (
     <div className="welcome-message">
         <h1 className="be-thoughtful">Be Thoughtful</h1>
-        <h1 className="signatures">S&B Card Signatures</h1>
-        <h1 className="slogan">We make card writing easier for all occasions</h1>
+        <h1 className="signature">S&B Card Signatures</h1>
+        <h1 className="slogan">
+            <p> We make card writing easy </p>
+            <p> for all occasions. </p>
+        </h1>
     </div>
     )
 }
