3a17a6e348802ebefcab0c4992826d508453f4c0
btambo <70358548+btambo@users.noreply.github.com>
2021-05-01 21:02:21 -0700

    Modify the animations for the front of the cards

--- src/Components/Cards/CardFront.js
+++ src/Components/Cards/CardFront.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 import './CardFront.css';
+
+const cardFrontVariants = {
+    hidden: {
+        opacity: 0,
+    },
+    show: {
+        opacity: 0.7,
+        transition: {
+            delay: 0.25,
+            duration: 1
+        }
+    },
+    hover: {
+        opacity: 1.1
+    }
+}
+   
 
 const CardFront = (props) => {
     const { title, link, frontMessage, handleClick } = props || {};
     return (
-        <div className='front-card-container' onClick={handleClick}>
+        <motion.div className='front-card-container' onClick={handleClick}
+            variants={cardFrontVariants}
+            initial="hidden"
+            animate="show"
+            whileHover="hover">
             <img src={link} alt=""/>
             <h4>{title}</h4>
             <p>{frontMessage}</p>
-        </div>
+        </motion.div>
     );
 }
 
