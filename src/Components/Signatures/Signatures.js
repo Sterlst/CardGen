21f8460835b7dd31b93dd9b11eac948d40d68742
btambo <70358548+btambo@users.noreply.github.com>
2021-05-01 22:13:09 -0700

    Initial set-up of Grid for Signatures

---
+++ src/Components/Signatures/Signatures.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './Signatures.css';
+
+const Signatures = () => {
+    return (
+
+        <div className='signatures-container'>
+            <p1 className='grid-item-1'>Dummy text 1</p1>
+            <p1 className='grid-item-2'>Dummy text 2</p1>
+            <p1 className='grid-item-3'>Dummy text 3</p1>
+            <p1 className='grid-item-4'>Dummy text 4</p1>
+            <p1 className='grid-item-5'>Dummy text 5</p1>
+            <p1 className='grid-item-6'>Dummy text 6</p1>
+            <p1 className='grid-item-7'>Dummy text 7</p1>
+        </div>
+
+    );
+}
+
+export default Signatures;
