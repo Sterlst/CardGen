2dbbad0e1ed34a40ecd56843d64f56a022a7246a
Sterling Tamboline <stamboli@sfu.ca>
2021-05-02 11:05:26 -0700

    Refactored card styling and added more space between nav links

--- src/Components/Cards/CardFront.js
+++ src/Components/Cards/CardFront.js
@@ -3,9 +3,9 @@ import React from 'react';
 import './CardFront.css';
 
 const CardFront = (props) => {
-    const { title, link, frontMessage, handleClick } = props || {};
+    const { title, link, frontMessage, handleCardClick } = props || {};
     return (
-        <div className='front-card-container' onClick={handleClick}>
+        <div className='front-card-container' onClick={handleCardClick}>
             <img src={link} alt=""/>
             <h4>{title}</h4>
             <p>{frontMessage}</p>
