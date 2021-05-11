9e323bf8d6205f5d2965e2a7c839b5135e427f03
btambo <70358548+btambo@users.noreply.github.com>
2021-05-10 20:57:09 -0700

    New Logo Size

--- src/Components/Navigationbar.js
+++ src/Components/Navigationbar.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import Logo from '../Images/Logo.svg';
-import HomeIcon from '../Images/Home.svg';
-import AboutIcon from '../Images/About.svg';
 
 import './Navigationbar.css';
 
@@ -16,18 +14,16 @@ const Navigationbar = () => {
                 /> {' '}
             </Navbar.Brand>
             <Navbar.Brand className='navbar-home ml-auto' href="/">
-                <img
-                    alt=""
-                    src={HomeIcon}
-                />{' '}
                 Home
             </Navbar.Brand>
-            <Navbar.Brand lassName='navbar-about ml-auto' href="/About">
-                <img
-                    alt=""
-                    src={AboutIcon}
-                />{' '}
+            <Navbar.Brand>
+                Upcoming
+            </Navbar.Brand>
+            <Navbar.Brand href="/About">
                 About
+            </Navbar.Brand>
+            <Navbar.Brand>
+                Contact
             </Navbar.Brand>
         </Navbar>
     )   
